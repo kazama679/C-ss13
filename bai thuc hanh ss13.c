e3cf8d7e4194951e1a2cbab93d56beac3ebebdd8
kazama679 <147390572+kazama679@users.noreply.github.com>
2023-11-29 12:42:15 +0700

    Add files via upload

---
+++ bai thuc hanh ss13.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<string.h> 
+int main(){
+	
+//	//VD1: 
+//	char str[] = "Hello world";
+//	printf("str[0] =%c\n", str[0]); 
+//	
+//	//VD2:
+//	char str[] = "Hello!";
+//	str[0] = 'Y';
+//	printf("%s", str);//in ra all 
+//	
+//	//VD3:
+//	char str[] = "Hello world";
+//	printf("%s\n", str);
+//	printf("%s\n", &str[5]);
+//	printf("%.4s\n", str);
+	
+//	VD4:
+//	char str[10];
+//	printf("Nhap mot chuoi ky tu ngau nhien:\n");
+//	scanf("%s", str);
+//	printf("%s", str); 
+
+//	//VD5:
+//	char str[10];
+//	printf("Nhap mot chuoi ky tu ngau nhien:\n");
+//	gets(str);
+//	puts(str); 
+	
+//	int num; 
+//	char str[100]; 
+//	printf("Nhap vao 1 so tu nhien bat ky\n");
+//	scanf("%d", &num);
+//	printf("So ban vua nhap la %d\n", num);
+//	printf("Nhap vao 1 chuoi ky tu bat ky\n");
+//	fflush(stdin); // xoa all ki tu  
+//	gets(str);
+//	puts(str); 
+
+//	// cach noi 2 ham lai voi nhau 
+//	char str1[10] = "bc";
+//	char str2[] = "bb";
+//	int result = strcmp(str1, str2);
+//	printf("%d", result); 
+////	strcmp(str1, str2); // so sánh
+////	strcat(str1, str2);
+////	puts(str1);
+	int n; 
+	char str1[] = "ab";
+	char str2[] = "cd";
+	strlen(str1)=n;
+	printf("%d", n);
+}
